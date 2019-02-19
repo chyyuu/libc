56391237910ec547a75305a5fb01b11b0cd5c5f5
chyyuu <yuchen@mail.tsinghua.edu.cn>
2019-02-19 16:57:06 +0800

    can compile and run on ubuntu 16.04 x86-64

--- src/test.c
+++ src/test.c
@@ -27,27 +27,27 @@ int main(int argc, char* argv[])
         strcpy(v[i], argv[i]);
     }
 
-    fp = fopen("test.txt", "w");
-    for(i = 0; i < argc; ++i){
-        int len = strlen(v[i]);
-        fwrite(&len, 1, sizeof(int), fp);
-        fwrite(v[i], 1, len, fp);
-    }
-    fclose(fp);
+    // fp = fopen("test.txt", "w");
+    // for(i = 0; i < argc; ++i){
+    //     int len = strlen(v[i]);
+    //     fwrite(&len, 1, sizeof(int), fp);
+    //     fwrite(v[i], 1, len, fp);
+    // }
+    // fclose(fp);
 
-    fp = fopen("test.txt", "r");
-    for(i=0; i < argc; ++i){
-        int len;
-        char* buf;
-        fread(&len, 1, sizeof(int), fp);
-        buf = malloc(len + 1);
-        fread(buf, 1, len, fp);
-        buf[len] = '\0';
-        printf("%d %s\n", len, buf);
-        free(buf);
-        free(v[i]);
-    }
-    fclose(fp);
+    // fp = fopen("test.txt", "r");
+    // for(i=0; i < argc; ++i){
+    //     int len;
+    //     char* buf;
+    //     fread(&len, 1, sizeof(int), fp);
+    //     buf = malloc(len + 1);
+    //     fread(buf, 1, len, fp);
+    //     buf[len] = '\0';
+    //     printf("%d %s\n", len, buf);
+    //     free(buf);
+    //     free(v[i]);
+    // }
+    // fclose(fp);
     printf("size of int=%d and size of char*=%d size of unsigned=%d size of double=%d size of char=%d\n", sizeof(int), sizeof(char*), sizeof(unsigned), sizeof(double), sizeof(char));
     int a[] = {2, 3};
     char* str[] = {"string1 helloworld long text","string2"};
